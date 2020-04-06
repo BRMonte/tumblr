338c3703221e5dc1691ef03cc021cb885184fac4
bruno montebello <brunormontebello@hotmail.com>
2020-04-06 12:44:30 -0300

    Create comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy #dstroy the comments when the post is deleted
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
 end
